e24a55e985825d7ea325300b5575021b9b68e160
Gagan <gagan.rajoria_cs.aiml24@gla.ac.in>
2026-08-06 15:36:59 +0530

    Time: 1 ms (100%), Space: 46.7 MB (70.44%) - LeetHub

---
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] limitOccurrences(int[] nums, int k) {
+        int n = nums.length;
+        int l = 1, cnt = 1;
+        for (int r = 1; r < n; r++) {
+            if (nums[r] != nums[r - 1]) {
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+            if (cnt <= k) {
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+        return Arrays.copyOf(nums, l);
+    }
+}
